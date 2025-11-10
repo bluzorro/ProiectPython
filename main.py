328bcd9649fbcbe14d421b8a1ab722dc321e63f1
bluzorro <revoc123bussiness@gmail.com>
2025-11-10 19:27:50 +0200

    Functii noi

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-listaCarti = [] # de introdus sau fetch-ed
-
 class Carte:
     def __init__(self, name, author, genre, launchYear, pages):
         self.name = name
@@ -23,12 +21,33 @@ class Library:
 
     def __filter_autor(author):
         # Filtered search dupa author, va afisa doar cartilor scrise de autorul respectiv
-        return author
+        listaCurenta.clear()
+        for carte in listaCarti:
+            if (author == carte.author):
+                listaCurenta.append(carte)
+
+        # Functie display/update display si afiseaza cartile care corespund filtrarii
 
 
     def __filter_genre(genre):
-        return genre # placeholder
+        listaCurenta.clear()
+        for carte in listaCarti:
+            if (genre == carte.genre):
+                listaCurenta.append(carte)
 
+        # Update display
+
+
+listaCarti = [
+    Carte("Poor Folk", "Fyodor Dostoyevsky", "Fiction", 1846, 271),
+    Carte("White Nights", "Fyodor Dostoyevsky", "Fiction", 1848, 120),
+    Carte("Crime and Punishment", "Fyodor Dostoyevsky", "Fiction", 1866, 671),
+    Carte("The Gambler", "Fyodor Dostoyevsky", "Fiction", 1867, 210),
+    Carte("The Idiot", "Fyodor Dostoyevsky", "Fiction", 1869, 656),
+
+]  # de introdus sau fetch-ed
+
+listaCurenta = [] # cartile ce vor fi afisate dupa search / filter
 
 # Concept menu screen
 
